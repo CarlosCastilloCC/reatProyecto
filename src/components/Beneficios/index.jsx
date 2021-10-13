490fc1594e215ce5843574b9d3afdb1e40e1a368
dahiana <nanaandrea2215@gmail.com>
2021-10-13 16:48:49 -0500

    correciones

--- src/components/Beneficios/index.jsx
+++ src/components/Beneficios/index.jsx
@@ -9,7 +9,7 @@ function Beneficios() {
                     <div className="col-lg-6 col-sm-12 col-md-12" >
                         <img  src={Salud} className="salud "    alt=""/>
                     </div>
-                    <div className="col-lg-6 col-sm-12 col-md-12">
+                    <div className="col-lg-6 col-sm-12 col-md-12 caja-beneficios">
                     <h2 className="titulo1 mt-4 ml-3 mb-3 text-center text-titulo">Beneficios</h2>
                      <p className=" titulo2 mb-3 text-muted  text-center text-texto">
                        <li className="li-1">Entrena en el horario que desees. </li> 
